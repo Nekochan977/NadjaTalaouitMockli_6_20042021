80db6edf580b3fe18bb58101822fc53e19abf3de
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2021-10-08 17:13:10 +0200

    récupération des tags

--- app.js
+++ app.js
@@ -1,54 +1,36 @@
 // HTML nodes
 
-const header = document.querySelector('#main-header');
-const logo = document.querySelector('.logo');
+// const header = document.querySelector('#main-header');
+// const logo = document.querySelector('.logo');
 
 // Getting JSON data
 
 fetch('./data.json')
-.then(function(response){
-    return response.json();
+.then(function(res){
+    return res.json();
 })
-.then(function(jsonData){
-    console.log(jsonData);
-    console.log(jsonData['media']);
-    console.log(jsonData['media'].length);
-
-})
- 
-// fetch('./data.json').then(function(res){
-//     return res.json();
-// }).then(function(data){
-//     console.log(data)
-// })
-// console.log(requestJson)
-
-// let toto = fetch('./data.json').then(function(res){
-//     return res.json();
-// }).then(function(data){JSON.parse(data)})
-// let toto;
-
-// fetch('./data.json').then(function(res){
-//     return res.json();
-// }).then(function(json){
-//     toto=json['photographers'][1]['id'];
-// })
-
-// console.log(toto)
-// console.log(toto.data['photographers'][1]['id'])
+.then(function(data){
+    // console.log(data);
     
-// requestJson.open('get')
-// Creating HTML elements through JavaScripts
-
-// Header
-    // Logo
-document.querySelector('.logo').innerHTML = '<a href="#" class="main-page-link"></a>';
-document.querySelector('.main-page-link').innerHTML = '<img src="./images/logo.png" alt="FishEye-logo">';
-
-    // Navbar
-document.querySelector('.nav-bar').innerHTML = '<div class="tag-list"></div>';
-
-
-// document.querySelector('.tag-list').append.innerHTML = '<a href="#" class"photograph-tag">#nature</a>';
-
-// document.querySelector('body').innerHTML = '<p>je suis un paragraphe venant de js</p>'
+    let tags = {}
+    data.photographers.forEach(el => {
+        el.tags.forEach(function(tag){
+            tags[tag]=1;
+        
+        })
+    });
+    
+    tags = Object.keys(tags);
+    console.log(tags);
+   
+    const dispTags = (tags, node) => {
+        tags.forEach(tag => {
+            node.innerHTML += `<span>${tag}</span>`;
+        })
+        
+        
+    }
+    dispTags(tags, document.querySelector('.nav-bar'));
+   
+    console.log(tags);
+})
