7b5a4008fc7e61c6dc155a2348bb50a5a6f417a7
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2021-10-18 23:48:05 +0200

    Modification de dispPhotographers pour avoir liens + séparation de chacun des tags

--- app.js
+++ app.js
@@ -1,81 +1,86 @@
-// HTML nodes
-
-// const header = document.querySelector('#main-header');
-// const logo = document.querySelector('.logo');
-
 // Getting JSON data
 
-fetch('./data.json')
-.then(function(res){
+fetch("./data.json")
+  .then(function (res) {
     return res.json();
-})
-.then(function(data){
+  })
+  .then(function (data) {
     const photographers = data.photographers;
-    
-    //creation new objects
+
+    //creation new tag objects
     let tags = {};
 
-
     //récupérer les éléments tags dans data de l'array photographers
-    data.photographers.forEach(el => {
-        //récupérer un de chaque tag
-        el.tags.forEach(function(tag){
-            tags[tag]=1; //revoir avec Nissim cette partie
-        })
+    data.photographers.forEach((el) => {
+      //récupérer un de chaque tag
+      el.tags.forEach(function (tag) {
+        // console.log(tag);
+        tags[tag] = 1; //revoir avec Nissim cette partie
+      });
     });
-    
+
     tags = Object.keys(tags);
-    console.log(tags);
-    
+    // console.log(tags);
+
     // créer un nouveau content html pour les tags
     const dispTags = (tags, node) => {
-        tags.forEach(tag => {
-            node.innerHTML += `<a href="#" class="tag__link"><span class ="tag__ink-tag">#${tag}</span></a>`;
-        })  
-    }
+      tags.forEach((tag) => {
+        node.innerHTML += `<a href="#" class="tag__links"><span class ="tag__link-tag">#${tag}</span></a>`;
+      });
+    };
     // faire apparaître les tags dans la nav-barre
-    dispTags(tags, document.querySelector('.nav-bar'));
+    dispTags(tags, document.querySelector(".nav-bar"));
+    // console.log(tags);
 
-    
+    const dispPhotographers = (photographers, node) => {
+      let str = "";
 
-    const dispPhotographers = (photographers, node)=>{
+      photographers.forEach((p) => {
+        console.log(p.tags);
 
-        
-        
-        let str = '';
-        photographers.forEach((p) =>{
-            let srcImage = `./images/samples/pPhotos${p.portrait}`
-            str += `
-        <article class="photographer__card">
-            <div class="photographer__img">
-                <img src=${srcImage}/>
-            </div>
-            <h2 class="photographer__name">
-                ${p.name}
-            </h2>
-            <p class="photographer__location">
-                ${p.city}, ${p.country}
-            </p>
-            <p class="photographer__devise">
-                ${p.tagline}
-            </p>
-            <p class="photographer__price">
-                ${p.price}€
-            </p>
+        let srcImage = `./images/samples/pPhotos${p.portrait}`;
+
+        str += `
+            <article class="photographer__card">
+                <a href="#" class="link__photographer">
+                    <div class="photographer__img">
+                        <img src=${srcImage}/>
+                    </div>
+                    <h2 class="photographer__name">
+                        ${p.name}
+                    </h2>
+                </a>
+                <p class="photographer__location">
+                    ${p.city}, ${p.country}
+                </p>
+                <p class="photographer__devise">
+                    ${p.tagline}
+                </p>
+                <p class="photographer__price">
+                    ${p.price}€
+                </p> `;
+        str += `
             <div class="photographer__tags">
-                ${p.tags}
-            </div>
+                
+            `;
+
+        p.tags.forEach((el) => {
+          console.log(el);
+          str += `<a href="#" class="tag__link"><spanclass ="tag__link-tag">#${el}</spanclass></a>`;
+        });
+
+        str += `
+                </div>
             
         
-        </article>
-        `
-        })
-        
-        node.innerHTML = str;
+            </article>
+            `;
+      });
 
-        
-    }
-
-    dispPhotographers(photographers, document.querySelector('.photographers__container'));
-    
-})
+      node.innerHTML = str;
+    };
+    dispPhotographers(
+      photographers,
+      document.querySelector(".photographers__container")
+    );
+  });
