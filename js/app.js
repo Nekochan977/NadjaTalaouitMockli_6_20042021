ce3c8717ab7e082c2185dedde107e37cf66045ce
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2021-11-01 10:08:41 +0100

    Avancées sur photographer template + suppression btn dans photographerPage => rajouté dynamiquement au travers de app.js

--- js/app.js
+++ js/app.js
@@ -8,13 +8,29 @@ new DataReader().getData().then((data) => {
     if(window.location.pathname.includes('/photographerPage.html')){
         const id = window.location.search.split("id=")[1];
         const x = data.photographers.filter(photographer => photographer.id == id);
-        const photographer__info = document.querySelector('.photographer__info');
-        const photographe__header = document.createElement('div');
+        const photographerInfo = document.querySelector('.photographer__info');
+        const photographeHeader = document.createElement('div');
+        photographeHeader.className += "photographer__header"
         // html template for photographer info
         let info = `
-            <h1>${x[0].name}</h1>`;
-            photographer__info.appendChild(photographe__header);
-            photographe__header.innerHTML = info;
+            <h1>${x[0].name}</h1>
+            <div class="photographer__img">
+                <img src="Medias/photoID/${x[0].portrait}">
+            </div>
+            <p class="photographer__location">
+                    ${x[0].city}, ${x[0].country}
+                </p>
+                <p class="photographer__devise">
+                    ${x[0].tagline}
+                </p>
+                    
+                <p class="photographer__price">
+                     ${x[0].price}€/jour
+                </p>
+                <button class="contact">Contactez-moi</button> 
+            `;
+            photographerInfo.appendChild(photographeHeader);
+            photographeHeader.innerHTML = info;
     }
     //Script section for homepage
     else {
