610de4480749568c09ab92a00dea3a0511c512b2
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2021-11-24 11:07:20 +0100

    creation of module with function for contact modal

---
+++ js/Contact.js
@@ -0,0 +1,114 @@
+"use strict";
+
+//form submission
+function ContactModal(photographerId) {
+  const modalBtn = document.querySelector(".contact");
+  const submitBtn = document.getElementById("submit-btn");
+  const modalBg = document.querySelector(".bground-contact");
+  const closeModal = document.querySelector(".close-contactForm");
+  const modalHeader = document.querySelector(".form-header");
+  const form = document.getElementById("contact-form");
+  modalBtn.addEventListener("click", launchModal);
+  closeModal.addEventListener("click", crossClose);
+  //launch Modal form
+  function launchModal() {
+    modalBg.style.display = "block";
+    modalBtn.style.display = "none";
+    let header = `<span class="header-content">${photographerId[0].name}</br> Contactez-moi</span>`;
+    modalHeader.innerHTML = header;
+  }
+  //close Modal form
+  function crossClose() {
+    modalBg.style.display = "none";
+    modalBtn.style.display = "block";
+  }
+  // Form validation
+
+  // Retrieve nodes for validation
+  //first name
+  const firstName = document.querySelector("#prenom");
+  const firstnameDiv = document.querySelector("#prenom-div");
+  //last name
+  const lastName = document.querySelector("#nom");
+  const lastnameDiv = document.querySelector("#nom-div");
+  //email
+  const email = document.querySelector("#email");
+  const emailDiv = document.querySelector("#email-div");
+  //message
+  const message = document.querySelector("#message");
+  const messageDiv = document.querySelector("#message-div");
+
+  submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    
+    //add a counter to check each form input
+    let counter = 0;
+    // Validation conditions
+
+    // input values does not accept blanks
+    const firstNameValue = firstName.value.trim();
+    const lastNameValue = lastName.value.trim();
+
+    //define email format
+    const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    // check firstname
+
+    if (firstNameValue.length < 2) {
+      // Error message
+      firstnameDiv.setAttribute(
+        "data-error",
+        "merci de mettre 2 caractères minimum"
+      );
+      firstnameDiv.setAttribute("data-error-visible", "true");
+    } else {
+      firstnameDiv.setAttribute("data-error-visible", "false");
+      counter++;
+    }
+    // check lastname
+
+    if (lastNameValue.length < 2) {
+      // Error message
+      lastnameDiv.setAttribute(
+        "data-error",
+        "merci de mettre 2 caractères minimum"
+      );
+      lastnameDiv.setAttribute("data-error-visible", "true");
+    } else {
+      lastnameDiv.setAttribute("data-error-visible", "false");
+      counter++;
+    }
+    // check email
+
+    if (mailFormat.test(email.value)) {
+      console.log(email.value);
+      emailDiv.setAttribute("data-error-visible", "false");
+      counter++;
+    } else {
+      emailDiv.setAttribute(
+        "data-error",
+        "merci de donner une adresse mail valide"
+      );
+      emailDiv.setAttribute("data-error-visible", "true");
+    }
+    if (message.value == "") {
+      messageDiv.setAttribute("data-error", `merci d'entrer votre message`);
+      messageDiv.setAttribute("data-error-visible", "true");
+    } else {
+      messageDiv.setAttribute("data-error-visible", "false");
+      counter++;
+    }
+    // reset form
+    const validate = () => {
+      form.reset();
+    };
+   
+    if (counter === 4) {
+      modalBg.style.display = "none";
+      console.log(firstName.value, lastName.value, email.value, message.value);
+      validate();
+      modalBtn.style.display = "block";
+    }
+  });
+}
+export {ContactModal};
