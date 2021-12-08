d16a3abc89a8d5c93e504f181945e2b726592d9e
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2021-12-08 10:53:55 +0100

    transfered function for lightbox in module

---
+++ js/Lightbox.js
@@ -0,0 +1,91 @@
+"use stric";
+
+export function showLightbox(title) {
+  //lightbox
+  //queryselectors
+  const header = document.getElementById("main-header");
+  const idSection = document.querySelector(".photographer__id__section");
+  const listbox = document.querySelector(".listbox_container");
+  const images = document.querySelectorAll(".photo"); //Returns nodeList of images
+  const lightboxContainer = document.getElementById("lightbox");
+  const slides = document.querySelector(".slides");
+  const closeLibobxBtn = document.querySelector(".close-lightbox");
+  const lightboxBtns = document.querySelectorAll(".lightbox-btn");
+  const gallerySection = document.querySelector(".gallery");
+ 
+  const lightbox = [];
+
+  let id = [];
+
+  lightboxContainer.classList.add("active");
+  gallerySection.style.display = "none";
+  header.style.display = "none";
+  idSection.style.display = "none";
+  listbox.style.display = "none";
+
+  images.forEach(function (element) {
+    lightbox.push({ title: element.title, src: element.src });
+  });
+
+  id = lightbox.findIndex((element) => element.title == title);
+  
+  let slideHTML = "";
+  while (slides.firstChild) {
+    slides.removeChild(slides.firstChild);
+  }
+
+  if (lightbox[id].src.includes("mp4")) {
+    slideHTML = `<video controls class="photo" title="${lightbox[id].title}" type="video/mp4" src="${lightbox[id].src}"></video>`;
+  } else {
+    slideHTML = `<img class="mp4" title="${lightbox[id].title}" src="${lightbox[id].src}">`;
+  }
+  slides.innerHTML += slideHTML;
+
+  const transitionSlidesLeft = () => {
+    id--;
+    while (slides.firstChild) {
+      slides.removeChild(slides.firstChild);
+    }
+    if (lightbox[id].src.includes("mp4")) {
+      slideHTML = `<video controls class="photo" title="${lightbox[id].title}" type="video/mp4" src="${lightbox[id].src}"></video>`;
+    } else {
+      slideHTML = `<img class="photo" title="${lightbox[id].title}" src="${lightbox[id].src}">`;
+    }
+
+    slides.innerHTML += slideHTML;
+  };
+  const transitionSlidesRight = () => {
+    id++;
+
+    while (slides.firstChild) {
+      slides.removeChild(slides.firstChild);
+    }
+    if (lightbox[id].src.includes("mp4")) {
+      slideHTML = `<video controls class="photo" title="${lightbox[id].title}" src="${lightbox[id].src}" type="video/mp4"></video>`;
+    } else {
+      slideHTML = `<img class="photo" title="${lightbox[id].title}" src="${lightbox[id].src}">`;
+    }
+
+    slides.innerHTML += slideHTML;
+  };
+
+  const transitionSlidesHandler = (moveItem) => {
+    moveItem.includes("prev")
+      ? transitionSlidesLeft()
+      : transitionSlidesRight();
+  };
+  lightboxBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      transitionSlidesHandler(e.currentTarget.id);
+    });
+  });
+
+  // close lightbox
+  closeLibobxBtn.addEventListener("click", () => {
+    lightboxContainer.classList.remove("active");
+    header.style.display = "block";
+    idSection.style.display = "flex";
+    listbox.style.display = "flex";
+    gallerySection.style.display = "grid";
+  });
+}
