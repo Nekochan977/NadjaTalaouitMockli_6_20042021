cf5841eb5a0085616f6eb06f240ba0ae1115ada9
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2021-12-08 13:50:30 +0100

    modification css for lightbox

--- js/Gallery.js
+++ js/Gallery.js
@@ -1,5 +1,6 @@
 "use strict";
 import { filterBy } from "./Filter.js";
+import { showLightbox } from "./Lightbox.js";
 
 export default class Gallery {
   displayGallery(medias, photographerId) {
@@ -22,20 +23,17 @@ export default class Gallery {
       let card = "";
       if ("video" in media) {
         card = `
-          <a class="open__lightbox">
+          <div class="open__lightbox">
             <div class="gallery__photo">
-            <video controls class="photo" title="${
-              media.title
-            }"><source src="Medias/${photographerId[0].name
-          .split(" ")[0]
-          .replace("-", " ")}/${media.video}" type="video/mp4"></video></div>
-          </a>
+            <video class="photo" title="${ media.title}" src="Medias/${photographerId[0].name.split(" ")[0].replace("-", " ")}/${media.video}" type="video/mp4"></video>
+            </div>
+          </div>
           <div class="photo__description">
             <p class="photo__title">${media.title}</p>
             <p class="photo__date">${media.date}</p>
             <div class="likes">
-            <p class="total__likes">${media.likes}</p>
-            <button class="like"><i class="fas fa-heart"></i></button>
+            <p class="photo__likes">${media.likes}</p>
+            <button class="like" aria-label="click to like"><i class="fas fa-heart"></i></button>
             </div>
           </div>
           `;
@@ -43,7 +41,7 @@ export default class Gallery {
       //else add html with img element
       else {
         card = `
-          <a class="open__lightbox">
+          <div class="open__lightbox">
             <div class="gallery__photo">
               <img class="photo" title="${
                 media.title
@@ -56,8 +54,8 @@ export default class Gallery {
             <p class="photo__title">${media.title}</p>
             <p class="photo__date">${media.date}</p>
             <div class="likes">
-            <p class="total__likes">${media.likes}</p>
-            <button class="like"><i class="fas fa-heart"></i></button>
+            <p class="photo__likes">${media.likes}</p>
+            <button class="like" aria-label="click to like"><i class="fas fa-heart"></i></button>
             </div>
           </div>
           `;
@@ -69,112 +67,46 @@ export default class Gallery {
 
       //html template for the photos
 
-      gallerySection.appendChild(photoArticle);
       photoArticle.innerHTML += card;
+      gallerySection.appendChild(photoArticle);
+
+      //event listerner to launch lightbox
+      const openModal = document.querySelectorAll(".open__lightbox");
+      //console.log(openModal);
       photoArticle.addEventListener("click", (e) => {
-        showLightbox(media.title);
+        console.log(e.target);
+        showLightbox(media.title, media.src);
       });
     });
     // End of gallery
 
+    //Filter Gallery
     const select = document.getElementById("listbox"); //select html
     select.addEventListener("change", filterBy);
 
-    //lightbox
-    //queryselectors
-    const header = document.getElementById("main-header");
-    const idSection = document.querySelector(".photographer__id__section");
-    const listbox = document.querySelector(".listbox_container");
-    const images = document.querySelectorAll(".photo"); //Returns nodeList of images
-    const lightboxContainer = document.getElementById("lightbox");
-    const slides = document.querySelector(".slides");
-    const closeLibobxBtn = document.querySelector(".close-lightbox");
-    const lightboxBtns = document.querySelectorAll(".lightbox-btn");
-    //const lightboxBtnRight = document.querySelector("next");
-    //const lightboxBtnLeft = document.querySelector("prev");
+    //get all likes 
+    //queryslectors
+    const likesAndPriceDiv = document.querySelector(".likesAndPrice");
+    const photoLikes = document.querySelectorAll(".photo__likes");
+    const likeBtn = document.querySelector('.like');
+    let likesArray = [];
+    
+    let likesAndPrice = "";
+    photoLikes.forEach((element)=>{
+      likesArray.push(parseInt(element.firstChild.data))
+    });
+    // console.log(likesArray);
+    likeBtn.addEventListener("click", (e)=> {
+      
+      //photoLikes++
+      console.log(e.target);
+    })
+    const totalLikes = likesArray.reduce((a,b)=>{return a+b});
+    likesAndPrice = `<p class="total__likes">${totalLikes}<i class="fas fa-heart"></i></p> <p class="price"></p>`;
+  
+    likesAndPriceDiv.innerHTML += likesAndPrice;
 
-    const lightbox = [];
-
-    //functions
-    function showLightbox(title) {
-      let id = [];
-
-      lightboxContainer.classList.add("active");
-      gallerySection.style.display = "none";
-      header.style.display = "none";
-      idSection.style.display = "none";
-      listbox.style.display = "none";
-
-      images.forEach(function (element) {
-        console.log(element);
-        lightbox.push({ title: element.title, src: element.src });
-      });
-
-      id = lightbox.findIndex((element) => element.title == title);
-
-      console.log(lightbox);
-      let slideHTML = "";
-      while (slides.firstChild) {
-        slides.removeChild(slides.firstChild);
-      }
-
-      console.log(lightbox[id].src);
-
-      if (lightbox[id].src.includes("mp4")) {
-        slideHTML = `<div><video controls width="320" height="240" class="photo" title="${lightbox[id].title}"><source src="${lightbox[id].src} type="video/mp4"></video></div>`;
-      } else {
-        slideHTML = `<img class="photo" title="${lightbox[id].title}" src="${lightbox[id].src}">`;
-      }
-      slides.innerHTML += slideHTML;
-
-      const transitionSlidesLeft = () => {
-        id--;
-        //console.log("left"+id);
-        while (slides.firstChild) {
-          slides.removeChild(slides.firstChild);
-        }
-        if (lightbox[id].src.includes("mp4")) {
-          slideHTML = `<video controls width="320" height="240" class="photo" title="${lightbox[id].title}"><source src="${lightbox[id].src} type="video/mp4"></video>`;
-        } else {
-          slideHTML = `<img class="photo" title="${lightbox[id].title}" src="${lightbox[id].src}">`;
-        }
-
-        slides.innerHTML += slideHTML;
-      };
-      const transitionSlidesRight = () => {
-        id++;
-
-        while (slides.firstChild) {
-          slides.removeChild(slides.firstChild);
-        }
-        if (lightbox[id].src.includes("mp4")) {
-          slideHTML = `<video controls width="320" height="240" class="photo" title="${lightbox[id].title}"><source src="${lightbox[id].src} type="video/mp4"></video>`;
-        } else {
-          slideHTML = `<img class="photo" title="${lightbox[id].title}" src="${lightbox[id].src}">`;
-        }
-
-        slides.innerHTML += slideHTML;
-      };
-
-      const transitionSlidesHandler = (moveItem) => {
-        moveItem.includes("prev")
-          ? transitionSlidesLeft()
-          : transitionSlidesRight();
-      };
-      lightboxBtns.forEach((btn) => {
-        btn.addEventListener("click", (e) => {
-          transitionSlidesHandler(e.currentTarget.id);
-        });
-      });
-
-      // close lightbox
-      closeLibobxBtn.addEventListener("click", () => {
-        lightboxContainer.classList.remove("active");
-        header.style.display = "block";
-        idSection.style.display = "flex";
-        listbox.style.display = "flex";
-        gallerySection.style.display = "grid";
-      });
-    }
+   
   }
+  
 }
